a96421843cfad1d3cccc3df9eceba936ea6dd63c
marco villanueva <villanuevamarco98@gmail.com>
2020-05-20 16:52:40 -0600

    quest model created

---
+++ Models/Quest.cs
@@ -0,0 +1,9 @@
+namespace NinjaQuest.Models{
+    public class NinjaQuest
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Completed { get; set; }
+    }
+}
