cdcec41d8b4bdd7d462cc3d5465d0334b29d7fc3
marco villanueva <villanuevamarco98@gmail.com>
2020-06-11 21:08:05 -0600

    repo in ninjass service

--- Services/NinjasServices.cs
+++ Services/NinjasServices.cs
@@ -4,10 +4,11 @@ namespace NinjaQuest.Services
 {
     public class NinjasService
     {
+        private readonly NinjaRepository _repo;
         public NinjasService(NinjaRepository repo)
 
         {
-            
+            _repo = repo;
         }
     }
 }
