edc8fe1b4c76640899092b95ffb9fecf2ea0c587
marco villanueva <villanuevamarco98@gmail.com>
2020-06-12 20:48:27 -0600

    ninja repo

--- Services/NinjasServices.cs
+++ Services/NinjasServices.cs
@@ -5,6 +5,7 @@ namespace NinjaQuest.Services
     public class NinjasService
     {
         private readonly NinjaRepository _repo;
+        //dependency injection; see startup.cs line 42
         public NinjasService(NinjaRepository repo)
 
         {
