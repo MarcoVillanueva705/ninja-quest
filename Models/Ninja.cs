aa1dc8e5184c4ff5b5e50e91f777f613a43c3ef8
marco villanueva <villanuevamarco98@gmail.com>
2020-06-14 17:14:35 -0600

    ninja model min max

--- Models/Ninja.cs
+++ Models/Ninja.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NinjaQuest.Models
 {
     public class Ninja
     {
         public int Id { get; set; }
+        [Required]
+        [MinLength(2)]
+        [MaxLength(7)]
         public string Name { get; set; }
     }
 }//created Ninja table in SQL
