d86583f99644b87b3d5de7ed22eb72847580344c
marco villanueva <villanuevamarco98@gmail.com>
2020-06-14 15:05:18 -0600

    get method added, prev com't out

--- Controllers/NinjasController.cs
+++ Controllers/NinjasController.cs
@@ -17,21 +17,28 @@ namespace NinjaQuest.Controllers
 
         //Get All
         [HttpGet]
+    //Get all ninjas
+        public ActionResult<IEnumerable<Ninja>> Get()
+        {
+            return Ok(_service.Find());
+        }
         //method set as void b/c not returning anything
+
+        // public void Get();
        
        //If you're going to return an ActionResult,
        //you have to state what type it's going to be
        //Here we want to iterate through type Quest, which
        //is an array of objects
-        public ActionResult <IEnumerable<Quest>> Get()
-        {
+        // public ActionResult <IEnumerable<Quest>> Get()
+        // {
             // return an array of type Quests
             //return Quests[]
 
             //returns status code 200, acccepts dynamic object
             //of any type
-            return Ok(new Quest[] { new Quest() });
-        }
+        //     return Ok(new Quest[] { new Quest() });
+        // }
         //Get One usually by ID
         //{id} is a route parameter
         [HttpGet("{id}")]
