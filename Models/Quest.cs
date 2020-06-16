0f751f427664139b492089478c73a55f9594d484
marco villanueva <villanuevamarco98@gmail.com>
2020-06-15 19:59:12 -0600

    quest model title required

--- Models/Quest.cs
+++ Models/Quest.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace NinjaQuest.Models{
     public class Quest
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string Description { get; set; }
         public bool Completed { get; set; }
