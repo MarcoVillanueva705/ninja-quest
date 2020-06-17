840e47710c200aa2452b95ece0d7b866533d7378
marco villanueva <villanuevamarco98@gmail.com>
2020-06-16 19:58:59 -0600

    adding quests controller

---
+++ Controllers/QuestsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace NinjaQuest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class QuestsController: ControllerBase
+    {
+        
+    }
+}
