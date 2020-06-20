a5f7c3a7279f27a669dcb8f76fc9a0e1f359b4c1
marco villanueva <villanuevamarco98@gmail.com>
2020-06-20 17:43:52 -0600

    qr repo

--- Services/QuestsServices.cs
+++ Services/QuestsServices.cs
@@ -1,7 +1,12 @@
 namespace NinjaQuest.Services{
     public class QuestsService
     {
+        private readonly QuestsRepository _repo;
 
+        public QuestsServices(QuestsRepository repo)
+        {
+            _repo = repo;
+        }
     }
     
 //fill out qs
