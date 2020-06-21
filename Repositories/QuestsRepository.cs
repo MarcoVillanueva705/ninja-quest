1ab816d3e81d088e58bcb3ae57273aacbbb4cfdb
marco villanueva <villanuevamarco98@gmail.com>
2020-06-21 17:50:33 -0600

    quest create in QR

--- Repositories/QuestsRepository.cs
+++ Repositories/QuestsRepository.cs
@@ -1,14 +1,27 @@
+using System.Data;
+using Dapper;
 using NinjaQuest.Models;
 
 namespace NinjaQuest.Repositories
 {
     public class QuestsRepository : IRepository<Quest>
     {
+        private readonly IDbConnection _db;
+
+        public QuestsRepository(IDbConnection db)
+        {
+            _db = db;
+        }
         //capital letter means implementation i.e. I or T
         //inherit from classes, but implement interfaces
         public Quest Create(Quest quest)
         {
-            throw new System.NotImplementedException();
+           int id =  _db.ExecuteScalar<int>(@"
+            INSERT INTO quests (title, description)
+            VALUES (@Title, @Description)
+            ",quest);
+            quest.Id = id;
+            return quest;
         }
     }
 }
