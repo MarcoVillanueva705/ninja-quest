a648c4f5b0c2fbac2bb6e99bcebc17a46b588d74
marco villanueva <villanuevamarco98@gmail.com>
2020-06-21 17:38:14 -0600

    IRepo in QR

--- Repositories/QuestsRepository.cs
+++ Repositories/QuestsRepository.cs
@@ -1,8 +1,14 @@
+using NinjaQuest.Models;
+
 namespace NinjaQuest.Repositories
 {
-    public class QuestsRepository
+    public class QuestsRepository : IRepository<Quest>
     {
         //capital letter means implementation i.e. I or T
         //inherit from classes, but implement interfaces
+        public Quest Create(Quest quest)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
