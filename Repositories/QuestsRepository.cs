ca24427cf68d5b41fb53de7c244a32a852c58dfc
marco villanueva <villanuevamarco98@gmail.com>
2020-06-20 18:16:57 -0600

    notes about IRepo in QR

--- Repositories/QuestsRepository.cs
+++ Repositories/QuestsRepository.cs
@@ -2,6 +2,7 @@ namespace NinjaQuest.Repositories
 {
     public class QuestsRepository
     {
-        
+        //capital letter means implementation i.e. I or T
+        //inherit from classes, but implement interfaces
     }
 }
