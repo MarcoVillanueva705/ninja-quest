9d55e18872c99499d4d8e233037373d14d6c9754
marco villanueva <villanuevamarco98@gmail.com>
2020-06-22 21:01:30 -0600

    IRepo methods

--- Repositories/IRepository.cs
+++ Repositories/IRepository.cs
@@ -1,8 +1,13 @@
+using System.Collections.Generic;
+
 namespace NinjaQuest.Repositories
 {
     //T means to be defined later
     public interface IRepository<T>
     {
        T Create(T t);
+       T FindById(int id);
+       List<T> Find();
+       bool Delete(int id);
     }
 }
