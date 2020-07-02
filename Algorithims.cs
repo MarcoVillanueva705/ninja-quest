8e84150f5f8f2304ae571fed2dfbe70896569b6b
marco villanueva <villanuevamarco98@gmail.com>
2020-07-01 20:37:34 -0600

    insert last method

--- Algorithims.cs
+++ Algorithims.cs
@@ -48,6 +48,21 @@ namespace SinglyLinkedList
             }
             Console.WriteLine();
         }
+        
+        public void insertLast(int data)
+        {
+            Node current = first;
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+
+            Node newNode = new Node();
+            {
+                newNode.data = data;
+                current.next = newNode;
+            }
+        }
     }
 
 
